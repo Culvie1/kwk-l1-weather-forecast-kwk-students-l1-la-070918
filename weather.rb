1e357bd4b1a5ace1b569f41d125a0e0c8ab84506
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-12 17:01:58 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -27,7 +27,7 @@ end
 #4. Write a method that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
-weather = []
+weather = [Burbank, Partly Cloudy, 82]
 weather.each do |a|
   The city is |a|
 
